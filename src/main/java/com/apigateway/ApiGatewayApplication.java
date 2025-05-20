5bf9eb96de0340fbcf6ccf17647a4943339eaa3e
py0o0 <pantom0425@naver.com>
2025-05-20 15:17:45 +0900

    Update ApiGatewayApplication.java

--- src/main/java/com/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -43,8 +43,8 @@ public class ApiGatewayApplication {
                         r -> r.path("/sse/**").uri("lb://alarm"))
                 .route("log",
                         r -> r.path("/logs/**").uri("lb://log"))
-                .route("eum-information", 
-                        r -> r.path("/information/**").uri("lb://eum-information"))
+                .route("information", 
+                        r -> r.path("/information/**").uri("lb://information"))
 
                 .route("EUM-CHATBOT",
                         r -> r.path("/api/v1/chatbot").uri("lb://EUM-CHATBOT"))
